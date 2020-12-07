1f17fa6b9b55b71079f476395cdaee3bc788c19f
Meghna Pancholi <mp832@cornell.edu>
2020-12-07 09:35:17 -0700

    Fixing symbol lookup.

--- design.sv
+++ design.sv
@@ -130,14 +130,15 @@ module Symbol_Lookup
 );
   always @* begin
     case(in)
-      3'b000 : out <= 3'b000;
-      3'b001 : out <= 3'b100;
-      3'b010 : out <= 3'b010;
-      3'b011 : out <= 3'b001;
-      3'b100 : out <= 3'b110;
-      3'b101 : out <= 3'b011;
-      3'b110 : out <= 3'b111;
-      default: out <= 3'b101;
+      3'd0 : out <= 3'b000;
+      3'd1 : out <= 3'b100;
+      3'd2 : out <= 3'b010;
+      3'd3 : out <= 3'b001;
+      3'd4 : out <= 3'b110;
+      3'd5 : out <= 3'b011;
+      3'd6 : out <= 3'b111;
+      3'd7 : out <= 3'b101;
+      default: out <= 3'b000;
     endcase
   end
 endmodule
@@ -149,15 +150,15 @@ module Index_Lookup
 );
   always @* begin
     case(in)
-      // NEED TO DOUBLE CHECK THESE REVERSE LOOKUPS D:!!
-      3'b000 : out <= 3'b000;
-      3'b001 : out <= 3'b011;
-      3'b010 : out <= 3'b010;
-      3'b011 : out <= 3'b101;
-      3'b100 : out <= 3'b001;
-      3'b101 : out <= 3'b011;
-      3'b110 : out <= 3'b100;     
-      default: out <= 3'b101;
+      3'b000 : out <= 3'd0;
+      3'b100 : out <= 3'd1;
+      3'b010 : out <= 3'd2;
+      3'b001 : out <= 3'd3;
+      3'b110 : out <= 3'd4;
+      3'b011 : out <= 3'd5;
+      3'b111 : out <= 3'd6;  
+      3'b101 : out <= 3'd7; 
+      default: out <= 3'd0;
     endcase
   end
 endmodule
