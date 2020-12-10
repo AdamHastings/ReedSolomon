e8691ede514211620ba4656fb51dc2e5363eb91a
adamhastings <itsadamhastings@gmail.com>
2020-12-10 01:49:10 -0500

    removed comments

--- 7-5/design_7-5.sv
+++ 7-5/design_7-5.sv
@@ -1,4 +1,3 @@
-//`include "symbols.v"
 `include "GF.v"
 `include "Compute_S.v"
 
@@ -7,32 +6,9 @@ module RS_Decoder (
   input       [(`N*`SYMBOL_WIDTH)-1:0] codeword,
   output	  [(`N*`SYMBOL_WIDTH)-1:0] corrected
 ); 
-  // is there a better way to breakup a longer codeword without having to have a line 
-  // for every symbol D:
-  wire [`SYMBOL_WIDTH-1:0] S1;	// v = codeword, x = 2 
-  wire [`SYMBOL_WIDTH-1:0] S2;	// v = codeword, x = 3
-  //wire S1_rdy, S2_rdy, corr_rdy;
-  
-  //RS_S_Calculator s1_calc (		// but how do you know S1 value is ready and went to STATE_DONE?
-  //  .clk(clk),					// need to output resp_rdy?
-  //  .reset(reset),
-  //  .v(codeword),
-  //  .x(`SYMBOL_WIDTH'd2),
-  //  .s(S1),
-  //  .resp_rdy(S1_rdy)
-  //);
-  
-  //RS_S_Calculator s2_calc (
-  //  .clk(clk),
-  //  .reset(reset),
-  //  .v(codeword),
-  //  .x(`SYMBOL_WIDTH'd3),
-  //  .s(S2),
-  //  .resp_rdy(S2_rdy)
-  //);
-  
-  //wire [`SYMBOL_WIDTH-1:0] as1;
-  //wire [`SYMBOL_WIDTH-1:0] as2;
+
+  wire [`SYMBOL_WIDTH-1:0] S1;
+  wire [`SYMBOL_WIDTH-1:0] S2;
   
   Compute_S1 comps1 (
     .v(codeword),
@@ -46,11 +22,6 @@ module RS_Decoder (
   
   wire [`SYMBOL_WIDTH-1:0] X1;
   wire [`SYMBOL_WIDTH-1:0] Y1;
-  //wire [(`N*`SYMBOL_WIDTH)-1:0] corrector_out;
-  //always @* begin
-  //assign corrected <= corrector_out;
-    //rdy <= corr_rdy;
-  //end
   
   GF_Divider dec_div(
     .in0(S2),
@@ -65,14 +36,10 @@ module RS_Decoder (
   );
   
   RS_Corrector dec_corr (
-    //.clk(clk),
-    //.enable(enable),
     .reset(reset),
     .codeword(codeword),
     .X1(X1),
     .Y1(Y1),
-    //.S1_rdy(S1_rdy),
-    //.S2_rdy(S2_rdy),
     .corrected(corrected)
   );
   
@@ -82,7 +49,6 @@ module RS_Corrector(
   input reset,
   input [(`N*`SYMBOL_WIDTH)-1:0] codeword,
   input [`SYMBOL_WIDTH-1:0] X1, Y1,
-  //input S1_rdy, S2_rdy,
   output reg [(`N*`SYMBOL_WIDTH)-1:0] corrected
 );
   wire [`SYMBOL_WIDTH-1:0] symbol_Y1;
@@ -109,18 +75,12 @@ module RS_Corrector(
   always @* begin
     if (reset) begin
       corrected <= 21'bx;
-      //corr_rdy <= 0;
-    end
-    else begin
-      //corr_rdy <= 1;
+    end else begin
       corrected <= codeword;
       if (X1 != 0) begin
         corrected[(X1-1) * `SYMBOL_WIDTH +: `SYMBOL_WIDTH] <= corrector;
       end
     end
-    //corrected <= {codeword[20:(22-((X1-1) * 3))], corrector, codeword[(17-((X1-1) * 3)):0]};
-    //((((codeword >> (21-((X1-1) * 3))) << 3) || corrector) << (18-((X1-1) * 3))) || codeword;
-    //corrected[21-((X1-1) * 3) +: 3] <= corrector;
   end
 endmodule
 
