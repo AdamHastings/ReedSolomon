f5b390fb1b1c899ef384d600db885c44d44f0bb8
adamhastings <itsadamhastings@gmail.com>
2021-01-27 21:31:19 -0500

    tb and design run to completion. no errors

--- 7-5/testbench_7-5.sv
+++ 7-5/testbench_7-5.sv
@@ -7,7 +7,7 @@ module top();
   end
   
   bit         reset = 1;
-  bit  [(`N*`SYMBOL_WIDTH)-1:0] codeword; // = 21'b000_001_110_011_001_111_100; 	//v = [0, 1, 6, 3, 1, 7, 4]
+  bit  [(`N*`SYMBOL_WIDTH)-1:0] codeword;
   logic [(`N*`SYMBOL_WIDTH)-1:0] dec_out;
   
   /* Instantiate the DUT */
@@ -17,35 +17,49 @@ module top();
     .corrected (dec_out)
   );
   
-  //  m = [0, 1, 6, 3, 3]
-  bit [(`K*`SYMBOL_WIDTH)-1:0] message; // = 15'b000_100_111_001_001;
+  bit [(`K*`SYMBOL_WIDTH)-1:0] message;
   bit [(`N*`SYMBOL_WIDTH)-1:0] untampered_codeword;
   
   /* Run the tests */
-  int num_tests = 100;
+  int num_tests = 200;
   int num_success=0;
   
   initial begin
-    //$dumpfile("dump.vcd");
-    //$dumpvars;
+  $dumpfile("dump.vcd");
+  $dumpvars;
+  #10000 $finish;
+end
+  
+  initial begin
+    
+    
     
     integer seed;
   	assign seed = 10;
     $srandom(seed);
     
+    $display("");
+    $display("-----------------------------------------------------------");
+    $display("                    Testing (7,5) RS Decoder               ");
+    $display("-----------------------------------------------------------");
+    $display("");
+    
+    //////////////////////////////////////////
     // Run testcases with untampered codewords
+    //////////////////////////////////////////
     
     for (int i=0; i<num_tests; i = i+1) begin
       @ (negedge clk);
       reset <= 1;
-      //message = createRandomMessage(`K*`SYMBOL_WIDTH);
       
       // Randomize the message
-      std::randomize(message);
+      std::randomize(message); // uncomment later
+      //message = 15'b000_100_111_001_001;
       
       // create an encoding for the message
-      //$display("creating codeword");
       untampered_codeword = createEncoding(message);
+      // = 21'b000_100_111_001_001_101_110; 
+ 
       //$display("message:  %b", message);
       //$display("codeword: %b", untampered_codeword);
       
@@ -57,29 +71,30 @@ module top();
       assert (dec_out == untampered_codeword) else begin
         $error("Untampered Error at %0t ns: ", $time/1000);
         $display("---------------------------------------");
-        $display("        Codeword: %b", codeword);
-        $display("        Expected: %b", untampered_codeword);
-        $display("        Received: %b", dec_out);
+        $display("        Codeword: %x", codeword);
+        $display("        Expected: %x", untampered_codeword);
+        $display("        Received: %x", dec_out);
 		$display("---------------------------------------");
         $display(" Number of passed tests: %0d", num_success);
         $finish;
       end
       num_success = num_success + 1;
     end
-    $display("---------------------------------------");
-    $display("No errors found in untampered codewords!");
-	$display("---------------------------------------");
-    $finish;
+    //$display("----------------------------------------------");
+    $display("Number of errors found in %0d untampered codewords: %0d", num_tests, num_tests - num_success);
+    //$display("----------------------------------------------");
     
-    // Run testcases with tampered codewords.
+    //////////////////////////////////////////
+    // Run testcases with   tampered codewords
+    //////////////////////////////////////////
+    
+    num_success = 0;
     
     for (int i=0; i<num_tests; i = i+1) begin
       @ (negedge clk);
       reset <= 1;
-      //message = createRandomMessage(`K*`SYMBOL_WIDTH);
       
       // Randomize the message
-      //$srandom(seed);
       std::randomize(message);
       
       // create an encoding for the message
@@ -94,21 +109,25 @@ module top();
       @(posedge clk);
       reset <= 0;
       @ (negedge clk);
-      assert (dec_out == untampered_codeword) else begin
+      assert (message == dec_out[20:6]) else begin
         $error("Tampered Error at %0t ns: ", $time/1000);
         $display("---------------------------------------");
-        $display("        Codeword: %b", codeword);
-        $display("        Expected: %b", untampered_codeword);
-        $display("        Received: %b", dec_out);
+        $display("        Codeword: %x", codeword);
+        $display("        Expected: %x", untampered_codeword);
+        $display("        Received: %x", dec_out);
 		$display("---------------------------------------");
         $display(" Number of passed tests: %0d", num_success);
         $finish;
       end
       num_success = num_success + 1;
     end
-    $display("---------------------------------------");
-    $display("No errors found in tampered codewords!");
-	$display("---------------------------------------");
+    //$display("----------------------------------------------");
+    $display("Number of errors found in %0d   tampered codewords: %0d", num_tests, num_tests - num_success);
+    //$display("----------------------------------------------");
+    $display("");
+    $display("All tests passed!");
+    $display("");
+    $display("");
     $finish;
     
   end
@@ -118,23 +137,14 @@ module top();
 function bit [`SYMBOL_WIDTH-1:0] getSymbol (input int a);
   bit [`SYMBOL_WIDTH-1:0] s;
   
-  if (a == 0) begin
-    s=3'b000;
-  end else if (a == 1) begin
-    s=3'b100;
-  end else if (a == 2) begin
-    s=3'b010;
-  end else if (a == 3) begin
-    s=3'b001;
-  end else if (a == 4) begin
-    s=3'b110;
-  end else if (a == 5) begin
-    s=3'b011;
-  end else if (a == 6) begin
-    s=3'b111;
-  end else if (a == 7) begin
-    s=3'b101;
-  end
+  if (a == 0) begin s=8'b000; end
+  else if (a == 1) begin s=8'b100; end
+  else if (a == 2) begin s=8'b010; end
+  else if (a == 3) begin s=8'b001; end
+  else if (a == 4) begin s=8'b110; end
+  else if (a == 5) begin s=8'b011; end
+  else if (a == 6) begin s=8'b111; end
+  else if (a == 7) begin s=8'b101; end
   
   return s;
 endfunction
@@ -154,24 +164,15 @@ function int getIndex (input bit [`SYMBOL_WIDTH-1:0] a);
   endcase
   */
   
-  if (a == 3'b000) begin
-    i=0;
-  end else if (a == 3'b100) begin
-    i=1;
-  end else if (a == 3'b010) begin
-    i=2;
-  end else if (a == 3'b001) begin
-    i=3;
-  end else if (a == 3'b110) begin
-    i=4;
-  end else if (a == 3'b011) begin
-    i=5;
-  end else if (a == 3'b111) begin
-    i=6;
-  end else if (a == 3'b101) begin
-    i=7;
-  end
-  
+  if (a == 8'b000) begin i=8'd0; end
+  else if (a == 3'b100) begin i=3'd1; end
+  else if (a == 3'b010) begin i=3'd2; end
+  else if (a == 3'b001) begin i=3'd3; end
+  else if (a == 3'b110) begin i=3'd4; end
+  else if (a == 3'b011) begin i=3'd5; end
+  else if (a == 3'b111) begin i=3'd6; end
+  else if (a == 3'b101) begin i=3'd7; end
+      
   return i;
 endfunction
 
@@ -209,37 +210,29 @@ endfunction
 
 
 function bit [(`N*`SYMBOL_WIDTH)-1:0] createEncoding(input bit[(`K*`SYMBOL_WIDTH)-1:0] message);
+    
+  bit [`SYMBOL_WIDTH-1:0] shiftreg0;
+  bit [`SYMBOL_WIDTH-1:0] shiftreg1;
+  bit [`SYMBOL_WIDTH-1:0] gen0 = 110; 
+  bit [`SYMBOL_WIDTH-1:0] gen1 = 011; 
+  bit [`SYMBOL_WIDTH-1:0] feedback;
   
-  bit[(`N*`SYMBOL_WIDTH)-1:0] codeword;
+  shiftreg0 = 3'b000;
+  shiftreg1 = 3'b000;
+  gen0 = 110; // 4
+  gen1 = 011; // 5
+  feedback = message[(`K*`SYMBOL_WIDTH)-1 -: `SYMBOL_WIDTH];
   
-  // TODO this is where we create the encoding
-  bit [`SYMBOL_WIDTH-1:0] shiftreg0 = 3'b000;
-  bit [`SYMBOL_WIDTH-1:0] shiftreg1 = 3'b000;
-  bit [`SYMBOL_WIDTH-1:0] gen0 = 110; // 4
-  bit [`SYMBOL_WIDTH-1:0] gen1 = 011; // 5
-  bit [`SYMBOL_WIDTH-1:0] temp;
-  
-  
-  for(int i=`N-1; i>=0; i=i-1) begin
-    shiftreg0 = GFmult(GFadd(message[((i+1)*`SYMBOL_WIDTH)-1 -: `SYMBOL_WIDTH], shiftreg1), gen0);
-    shiftreg1 = GFadd(shiftreg0, GFmult(message[((i+1)*`SYMBOL_WIDTH)-1 -: `SYMBOL_WIDTH], gen1));
-    //$display("sr0: %b, sr1: %b", shiftreg0, shiftreg1);
+  for(int i=`K-1; i>=0; i=i-1) begin
+    //$display("sr0: %b, sr1: %b, feedback: %b", shiftreg0, shiftreg1, feedback);
+    shiftreg1 = GFadd(shiftreg0, GFmult(gen1, feedback));
+    shiftreg0 = GFmult(gen0, feedback);
+    feedback  = GFadd(shiftreg1, message[(i*`SYMBOL_WIDTH)-1 -: `SYMBOL_WIDTH]);
   end
+  //$display("sr0: %b, sr1: %b, feedback: %b", shiftreg0, shiftreg1, feedback);
   
-  codeword[(`N*`SYMBOL_WIDTH)-1:2*`SYMBOL_WIDTH] = message;
-  
-  // $display("shiftreg1: %b", shiftreg1);
-  codeword[2*`SYMBOL_WIDTH-1:`SYMBOL_WIDTH] = shiftreg1;
-  
-  
-  temp = shiftreg0;
-  shiftreg0 = GFmult(GFadd(0, shiftreg1), gen0);
-  shiftreg1 = GFadd(temp, GFmult(shiftreg1, gen1));
-  
-  codeword[`SYMBOL_WIDTH-1:0] = shiftreg1;
-  
-  return codeword;
-  
+  return {message, shiftreg1, shiftreg0};
+    
 endfunction
 
 function bit [(`N*`SYMBOL_WIDTH)-1:0] tamperCodeword(input bit [(`N*`SYMBOL_WIDTH)-1:0] untampered_codeword);
